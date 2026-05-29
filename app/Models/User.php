69c88abe4a891d041b47cebb68dab6c5a406fffd
ngochua1210 <ngochua2006@gmail.com>
2026-05-29 10:24:31 +0700

    api dang nhap dang ki dang xuat

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,13 +9,16 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens; // 1. Import class HasApiTokens
 
-#[Fillable(['name', 'email', 'password'])]
+// 2. Thêm 'role' và 'status' vào danh sách cho phép fill
+#[Fillable(['name', 'email', 'password', 'role', 'status'])] 
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    // 3. Khai báo sử dụng trait HasApiTokens
+    use HasApiTokens, HasFactory, Notifiable; 
 
     /**
      * Get the attributes that should be cast.
