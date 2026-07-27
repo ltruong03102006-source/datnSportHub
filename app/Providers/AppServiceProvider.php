e5beec1e66f3fa6434d29bbcaa63d27a9406da72
ngochua1210 <ngochua2006@gmail.com>
2026-07-27 12:44:23 +0700

    thiết lập kiến trúc Settlement Gateway cho luồng đối soát

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(\App\Gateways\SettlementGatewayInterface::class, \App\Gateways\SimulatedSettlementGateway::class);
     }
 
     /**
